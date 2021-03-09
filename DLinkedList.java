c3bb36ad9ca38bed9d24f1170911a645b76ba3e9
anonymousdomain <domainanonymous1@gmail.com>
2021-03-09 15:45:53 +0300

    atInsert implimented

--- DLinkedList.java
+++ DLinkedList.java
@@ -6,16 +6,18 @@ public class DLinkedList {
   int size;
 
   public DLinkedList() {
-    size=0;
+    size = 0;
     head = tail = null;
   }
 
   public boolean isEmpty() {
     return head == null;
   }
-  public int getSize(){
+
+  public int getSize() {
     return size;
   }
+
   public void addToHead(String data) {
     if (head == null) {
       head = new DLinkedListNode(null, data, null); //prev & next are null
@@ -40,56 +42,74 @@ public class DLinkedList {
     }
     size++;
   }
-public void removeFromHead(){
-    if(head==null)return;
-    else{
-        head=head.next;//shift the head to the next node
-        head.prev=null;//detach it from the link
-    }
-    size--;
-}
-public void removeFromTail(){
-    if(head==null)return;
-    else{
-        DLinkedListNode node=head;
-        while(node.next.next!=null){
-            node=node.next;
-            
-        }
-       node.next=null;//detach the last node
-    }
-    size--;
-}
-public void removeAt(int index){
-  if(head==tail || index>size)return;
- 
-  int i=0;//list starts from zero
-  DLinkedListNode node=head;
-  while(i<index){
-    node=node.next;
-    i++;
 
+  public void insertAt(String data, int index) {
+    if (head == tail || index > size) return;
+    int i = 0;
+    DLinkedListNode node = head;
+    while (i < index) {
+      node = node.next;
+      i++;
+    }
+    if (node.prev == null) {
+      DLinkedListNode newNode= new DLinkedListNode(null, data, node);
+      node.prev = newNode;
+     head=newNode;
+    } else {
+      DLinkedListNode newNode= new DLinkedListNode(node.prev, data, node);
+      node.prev.next=newNode;
+      node.prev=newNode;
+    }
+    size++;
   }
-if(node.next==null){
-  node.prev.next=null;
-}else if(node.prev==null)
-{
-  node=node.next;
-  node.prev=null;
-  head=node;
 
-}else{
-  node.prev.next=node.next;
-  node.next.prev=node.prev;
-}
-size--;
-}
+  public void removeFromHead() {
+    if (head == null) return; else {
+      head = head.next; //shift the head to the next node
+      head.prev = null; //detach it from the link
+    }
+    size--;
+  }
+
+  public void removeFromTail() {
+    if (head == null) return; else {
+      DLinkedListNode node = head;
+      while (node.next.next != null) {
+        node = node.next;
+      }
+      node.next = null; //detach the last node
+    }
+    size--;
+  }
+
+  public void removeAt(int index) {
+    if (head == tail || index > size) return;
+
+    int i = 0; //list starts from zero
+    DLinkedListNode node = head;
+    while (i < index) {
+      node = node.next;
+      i++;
+    }
+    if (node.next == null) {
+      node.prev.next = null;
+    } else if (node.prev == null) {
+      node = node.next;
+      node.prev = null;
+      head = node;
+    } else {
+      node.prev.next = node.next;
+      node.next.prev = node.prev;
+    }
+    size--;
+  }
+
   public void printList() {
     DLinkedListNode node = head; //node starting from head
     while (node != null) {
-      System.out.println(node.data);
+      System.out.print(node.data+" ");
       node = node.next; //itrate through the list
     }
+    System.out.println();
   }
- 
 }
--- DLinkedListTest.java
+++ DLinkedListTest.java
@@ -1,26 +1,32 @@
 package DoublyLinkedList;
 
 public class DLinkedListTest {
-    public static void main(String[] args) {
-        DLinkedList dlist=new DLinkedList();
-        //add to head
-        dlist.addToHead("Nahom");
-        dlist.addToHead("Devo");
-        dlist.printList();
-        //add to tail
-        System.out.println("###############");
-        dlist.addToTail("Neba");
-        dlist.addToTail("beka");
-        dlist.printList();
-        System.out.println("size : "+dlist.getSize());
-        //dlist.removeFromHead();
-        System.out.println("###############");
-       
-        dlist.removeFromTail();
-        System.out.println("size : "+dlist.getSize());
-        dlist.removeAt(0);
-        dlist.printList();
 
-        System.out.println("size : "+dlist.getSize());
-    }
+  public static void main(String[] args) {
+    DLinkedList dlist = new DLinkedList();
+    //add to head
+    dlist.addToHead("Nahom");
+    dlist.addToHead("Devo");
+    dlist.printList();
+    //add to tail
+    System.out.println("###############");
+    dlist.addToTail("Neba");
+    dlist.addToTail("beka");
+    dlist.printList();
+    System.out.println("size : " + dlist.getSize());
+    //add at any index
+    dlist.insertAt("bura",3);
+    dlist.insertAt("nati",2);
+    dlist.printList();
+    System.out.println("size : " + dlist.getSize());
+    System.out.println("###############");
+    dlist.removeFromTail();
+    System.out.println("size : " + dlist.getSize());
+    //remove from any index
+    dlist.removeAt(2);
+    dlist.removeAt(3);
+    dlist.printList();
+
+    System.out.println("size : " + dlist.getSize());
+  }
 }
